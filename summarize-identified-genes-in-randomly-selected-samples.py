d79d92b9755ae91001ccf09f112f31b06a29f285
yulinwang605 <50190876+yulinwang605@users.noreply.github.com>
2021-06-01 17:34:51 +0800

    Create summarize-identified-genes-in-randomly-selected-samples.py

---
+++ summarize-identified-genes-in-randomly-selected-samples.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python2
+######################################################################################################################################
+#    The purpose of this script is to summarize the rarefaction curve of detected genes vs the number randomly selected samples.     #
+#    CoverM (https://github.com/wwood/CoverM) was used to estimate the coverage values of genes in the reconstructed gene catalog    #
+#    among different AS metagenomic data.                                                                                            #
+#    If the gene coverage >= the provided cutoff, this gene would be deemed identified                                               #
+#                                                                                                                                    #
+#    Example files for input can be found in the folder of "example-data"                                                            #
+#    This script was written and tested in python 2.7                                                                                #
+######################################################################################################################################
+__email__       = "yulinwang605@gmail.com"
+
+import os
+import random
+import time
+
+import sys, getopt
+
+opts, args = getopt.getopt(sys.argv[1:],"hf:c:p:",["folder=","coverage=","permution="])
+input_file=""
+output_file=""
+def Usage():
+    print ""
+    print "Summarize identified genes in randomly select samples for the rarefaction curve"
+    print ""
+    print "-f   : Folder contains all CoverM (https://github.com/wwood/CoverM) results (contig-mode) of all all sample.."
+    print "-c,  : coverage cutoff for the valid identified genes in each sample."
+    print "-p   : times of permution."
+    print "-h,  : Print help"
+    print ""
+    print ""
+def finish():
+    print""
+    print"Job finidhed!"
+for op, value in opts:
+    if op == "-f":
+        folder = value
+        print "The folder of coverm results is ", folder
+    elif op == "-c":
+        filtercov = value
+        print "Coverage cutoff is ", filtercov
+    elif op == "-p":
+        pm = value
+        print "Use ",pm," times permution"
+    elif op == "-h":
+        Usage()
+        sys.exit()
+
+relation = open("corresponding.txt","r")
+#folder = raw_input("Enter the folder that contains all coverm results: ")
+output = open("samples-number-vs-identified-genes-"+str(pm)+"-permution.txt","w")
+#filtercov = raw_input("Enter the cufoff for average coverage: ")
+
+output.write("Sample-names"+"\t"+"Number-of-samples"+"\t"+"Identified-genes"+"\n")
+
+start = time.time()
+
+
+#prefilter the coverm results to rm the undetected files
+if os.path.exists("./filtered-coverm-results"):
+    print "Coverm results have been filtered."
+else:
+    os.mkdir("./filtered-coverm-results")
+    for root,dirs,files in os.walk(folder):
+        for file in files:
+            tmp1 = open(os.path.join(root,file),"r")
+            output1 = open("./filtered-coverm-results/"+str(file),"w")
+            for line in tmp1:
+                if "Sample" in str(line):
+                    output1.write(str(line).strip()+"\n")
+                elif float(str(line).split("\t")[2]) >= float(filtercov):
+                    output1.write(str(line).strip()+"\n")
+                else:
+                    continue
+            output1.close()
+
+
+#store the sample umber and sample name
+idName ={}
+for line in relation:
+    cont = str(line).strip().split("\t")
+    idName[cont[0]] = str(cont[1])+".coverage.txt"
+
+#creat a huge library to store the filename vs all identified genes.
+allgenes = {}
+for root,dirs,files in os.walk("./filtered-coverm-results"):
+    for file in files:
+        tmp2 = open(os.path.join(root,file),"r")
+        tmplist = []
+        for line in tmp2:
+            if "Sample" not in str(line):
+                tmplist.append(str(line).strip().split("\t")[1])
+            else:
+                continue
+        allgenes[str(file).strip()] = tmplist[0]+"\t"+"\t".join(tmplist[1:])
+print allgenes
+
+#randomly select samples
+ids = list(range(1,99,1)) #sample number from 1 to 6,this is for the test dataset
+#the following script will be used to study the random select samples numbersfor item in ids:
+for item in ids:
+    for i in range(0,int(pm),1): #randomly select number of times.
+        select = random.sample(ids,int(item))
+        i+=1
+        print "randamly select ",item,"samples"
+        filename = []
+        genename = [] #store identified gene name in different sample combination strategy
+        for sampleid in select:     #get number of id to tranfer into file name
+            filename.append(idName[str(sampleid)])
+        for name in filename:
+            genename = genename+str(allgenes[str(name)]).split("\t")
+        uniqgname = list(set(genename))#rm redundant gene names
+        if len(filename) == 1:
+            output.write(filename[0]+"\t"+str(item)+"\t"+str(len(uniqgname))+"\n")
+        else:
+            output.write(filename[0]+" "+" ".join(filename[1:])+"\t"+str(item)+"\t"+str(len(uniqgname))+"\n")
+
+output.close()
+
+
+end = time.time()
+print "Used time: ",str(end-start)
+
+finish()
+sys.exit()
